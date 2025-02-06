8fd133ebb9bdc405e9bf3ff6915dd1b2fbc744b2
Brandon <robles.brandon40@gmail.com>
2025-02-05 18:55:12 -0600

    Implementation of TestCases for White box and header description

--- src/test/java/MakeGuessTest.java
+++ src/test/java/MakeGuessTest.java
@@ -7,12 +7,56 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MakeGuessTest {
     
     private Game game = new Game();
-    
+
+    /**
+     * Tests the input of the Correct word chosen as first guess.
+     */
     @Test
     public void correctGuess() {
         game.initGame("horse", "Zach");
         double result = game.makeGuess("horse");
         assertEquals(0, result, 0.0001);
     }
-    
+
+    /**
+     * Tests the input if no guess is given
+     */
+    @Test
+    void testCountCorrectLetters_EmptyGuesses() {
+        Game game = new Game("lion", "Dr. M");
+        assertEquals(0, game.countCorrectLetters(), "Should return 0 if guesses list is empty.");
+    }
+    /**
+     * Tests the input if each correct letter is guessed individually
+     */
+    @Test
+    void testCountCorrectLetters_AllCorrect() {
+        Game game = new Game("lion", "Dr. M");
+        game.makeGuess("l");
+        game.makeGuess("i");
+        game.makeGuess("o");
+        game.makeGuess("n");
+        assertEquals(4, game.countCorrectLetters(), "Should return 4 if guesses list is empty.");
+    }
+    /**
+     * Tests the input of letters if only a few correct letters are guessed
+     */
+    @Test
+    void testCountCorrectLetters_SomeCorrect(){
+        Game game = new Game("lion", "Dr. M");
+        game.makeGuess("l");
+        game.makeGuess("i");
+        assertEquals(2,game.countCorrectLetters(), "Should return 2 if guesses list is empty.");
+    }
+    /**
+     * Tests the input if there are correct and incorrect letters guessed
+     */
+    @Test
+    void testCountCorrectLetters_Mix(){
+        Game game = new Game("lion", "Dr. M");
+        game.makeGuess("l");
+        game.makeGuess("j");
+        game.makeGuess("o");
+        assertEquals(2, game.countCorrectLetters(), "Should return 2 for correct guesses");
+    }
 }
