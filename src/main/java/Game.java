bfd1a21093df290566d1befb56b22579da9d5511
Brandon <robles.brandon40@gmail.com>
2025-02-11 19:45:05 -0600

    Style Fix

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -103,8 +103,8 @@ public class Game {
                 } else {
                     System.out.print('_');
                 }
-            }System.out.println();
-        }
+            }
+            System.out.println();}
         else {
             return 0;
         }
@@ -170,42 +170,37 @@ public class Game {
     }
 
     /**
-     * Checks if the guess made is correct, should ignore upper/lower case. Should give points based on made guess.
+     * Checks if the guess made is correct, should ignore upper/lower case.
+     * Should give points based on made guess.
      * Method returns a double, number of the double has different meanings
      * 0 Correct guess
      * 1.x Letter is in the word, x represents the number of times the letter is in the word
      * 2.0 Guess has correct length
      * 2.1 Guess is too long (only if it was a word)
      * 2.2 Guess is too short (only if it was a word)
-     * 3.0 Guess is partially included in the word (only if it was a word), given instead of 2.2 if it is a partial word
+     * 3.0 Guess is partially included in the word (only if it was a word),
+     * given instead of 2.2 if it is a partial word.
      * 4.0 This guess was already used
      * 4.1 Guess includes symbols, numbers (not just letters or one letter)
      * 5. After 10 guesses the game ends and is set to game over
-     * 5.1 If the player keeps guessing even though the status is not InProgress<p>
-     *
-     * The returned answer and the guess needs to be added to the respective lists for tracking.<p>
-     *
+     * 5.1 If the player keeps guessing even though the status is not InProgress
+     * <p>The returned answer and the guess needs to be added to the respective lists for tracking.<p>
      * If letter:
      * Return 1.NumOfOccurrence, 1.0 for the letter not being in the word, 1.1 for being in there once etc.
      * Add points according to NumOfOccurance
-     * <p>
-     *
-     * If word (go by this order of checks):
+     * <p>If word (go by this order of checks):
      * If word is correct return 0.0 and add points based on the length of the word (e.g. dog - 3 points,
      * horse - 5 points), set game status to won
      * If word is incorrect but has correct length return 2.0 and add 1 point
      * If word is incorrect and is partially included in the word return 3.0 and add 2 points
      * If word is too long or too short return 2.1, 2.2 accordingly and reduce points based on how off the word is
-     * (e.g. how many letters off)
-     * <p>
+     * (e.g. how many letters off)<p>
      * For either:
      * Guess was already used, reduce points by 2 and return 4.0 (checked before 4.1 error). Guess still counts toward
      * made guesses.
      * Guess includes numbers/symbols etc. (so more than just letters) reduce points by 3 and return 4.1, the guess is
-     * still added to the list of guesses
-     * <p>
-     * <p>
-     * Score can also be negative, that is no problem.
+     * still added to the list of guesses<p>
+     * <p>Score can also be negative, that is no problem.
      * When the player guessed 10 times and did not guess the word set the game to game over (status) and return 5.0
      * (no matter if there was another error).
      * If the player guesses again, even though game status is won or game over return 5.1.
