09e1e52aedb1ecaab54adec5cc91b6aeb082c9bd
Brandon <robles.brandon40@gmail.com>
2025-02-11 19:36:55 -0600

    Style Fix

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 /**
  * Class for handling some game logic for hangman game.
- * Every game starts with a score of 10 and the points are reduced or increased based on the description of "makeGuess". Points holds the current score for one game.
+ * Every game starts with a score of 10 and the points are reduced or increased based on the description of "makeGuess".
+ * Points holds the current score for one game.
  * Game is lost when the user made 10 guesses and did not guess the word.
  */
 public class Game {
@@ -54,7 +55,7 @@ public class Game {
     }
 
     /**
-     * Gets the answer for the game and puts it lowecase
+     * Gets the answer for the game and puts it lowercase.
      *
      * @return String The Answer.
      */
@@ -74,7 +75,6 @@ public class Game {
     /**
      * Sets the score for the game.
      *
-     * @param points
      */
     public void setPoints(int points) {
         this.points = points;
@@ -104,7 +104,10 @@ public class Game {
                 }
             }
             System.out.println();
-        } else return 0;
+        }
+        else {
+            return 0;
+        }
         return result;
     }
 
@@ -138,7 +141,6 @@ public class Game {
     /**
      * Constructs a new game with a given word and given name.
      *
-     * @param name
      */
     public Game(String fixedWord, String name) {
         this.name = "Anna";
@@ -181,28 +183,35 @@ public class Game {
      * 5. After 10 guesses the game ends and is set to game over
      * 5.1 If the player keeps guessing even though the status is not InProgress
      * <p>
+     *
      * The returned answer and the guess needs to be added to the respective lists for tracking.
      * <p>
+     *
      * If letter:
      * Return 1.NumOfOccurrence, 1.0 for the letter not being in the word, 1.1 for being in there once etc.
      * Add points according to NumOfOccurance
      * <p>
+     *
      * If word (go by this order of checks):
-     * If word is correct return 0.0 and add points based on the length of the word (e.g. dog - 3 points, horse - 5 points), set game status to won
+     * If word is correct return 0.0 and add points based on the length of the word (e.g. dog - 3 points,
+     * horse - 5 points), set game status to won
      * If word is incorrect but has correct length return 2.0 and add 1 point
      * If word is incorrect and is partially included in the word return 3.0 and add 2 points
-     * If word is too long or too short return 2.1, 2.2 accordingly and reduce points based on how off the word is (e.g. how many letters off)
+     * If word is too long or too short return 2.1, 2.2 accordingly and reduce points based on how off the word is
+     * (e.g. how many letters off)
      * <p>
      * For either:
-     * Guess was already used, reduce points by 2 and return 4.0 (checked before 4.1 error). Guess still counts toward made guesses.
-     * Guess includes numbers/symbols etc. (so more than just letters) reduce points by 3 and return 4.1, the guess is still added to the list of guesses
+     * Guess was already used, reduce points by 2 and return 4.0 (checked before 4.1 error). Guess still counts toward
+     * made guesses.
+     * Guess includes numbers/symbols etc. (so more than just letters) reduce points by 3 and return 4.1, the guess is
+     * still added to the list of guesses
      * <p>
      * <p>
      * Score can also be negative, that is no problem.
-     * When the player guessed 10 times and did not guess the word set the game to game over (status) and return 5.0 (no matter if there was another error).
+     * When the player guessed 10 times and did not guess the word set the game to game over (status) and return 5.0
+     * (no matter if there was another error).
      * If the player guesses again, even though game status is won or game over return 5.1.
      *
-     * @param guess
      * @return double returns the appropriate number
      */
     public double makeGuess(String guess) {
